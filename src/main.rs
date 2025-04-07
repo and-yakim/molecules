e02fff89ee87beaaf19776fdd068211a0f34f532
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-04-07 03:16:59 +0300

    use macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use molecules::molecule::*;
+use std::time;
+
+#[macroquad::main("Molecules")]
+async fn main() {
+    if let Ok(n) = time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH) {
+        rand::srand(n.as_secs());
+    }
+
+    loop {
+        clear_background(DARKGRAY);
+
+        next_frame().await;
+    }
 }
---
+++ src/molecule.rs
@@ -0,0 +1,10 @@
+pub use macroquad::prelude::*;
+
+pub struct Molecule {
+    pos: Vec2,
+    vel: Vec2,
+}
+
+impl Molecule {
+    fn new() {}
+}
