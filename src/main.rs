ddc5f4e6b4dd86f384962f8686e682e172e77d50
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-04-08 19:01:22 +0300

    mol methods prototypes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use molecules::molecule::*;
 use std::time;
 
-#[macroquad::main("Molecules")]
-async fn main() {
+const BORDER: f32 = 40.0;
+
+fn init() -> Camera2D {
     if let Ok(n) = time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH) {
         rand::srand(n.as_secs());
     }
+    request_new_screen_size(SIDE + BORDER, SIDE + BORDER);
+    Camera2D {
+        target: Vec2::ZERO,
+        zoom: 2.0 * Vec2::ONE / vec2(SIDE + BORDER, SIDE + BORDER),
+        ..Default::default()
+    }
+}
+
+#[macroquad::main("Molecules")]
+async fn main() {
+    let camera = init();
+
+    let mut gas = vec![Molecule::new(Vec2::ZERO, Vec2::ZERO)];
 
     loop {
         clear_background(DARKGRAY);
+        set_camera(&camera);
 
+        gas[0].draw();
+
+        draw_rectangle_lines(-HALF, -HALF, SIDE, SIDE, 2.0, GREEN);
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
         next_frame().await;
     }
 }
--- src/molecule.rs
+++ src/molecule.rs
@@ -1,10 +1,43 @@
 pub use macroquad::prelude::*;
 
+pub const SIDE: f32 = 800.0;
+pub const HALF: f32 = SIDE / 2.0;
+
+pub const RADIUS: f32 = 20.0;
+
+const fn pow(num: f32, exp: usize) -> f32 {
+    if exp <= 1 {
+        num
+    } else {
+        num * pow(num, exp - 1)
+    }
+}
+
+const R2: f32 = pow(RADIUS, 2);
+const RC: f32 = RADIUS * 2.5;
+const RC2: f32 = pow(RC, 2);
+
+const FC: f32 = -(-1.0 / pow(2.5, 14) + 1.0 / pow(2.5, 8));
+
 pub struct Molecule {
-    pos: Vec2,
-    vel: Vec2,
+    pub pos: Vec2,
+    pub vel: Vec2,
 }
 
 impl Molecule {
-    fn new() {}
+    pub fn new(pos: Vec2, vel: Vec2) -> Molecule {
+        Molecule { pos, vel }
+    }
+
+    pub fn force(&mut self, mol: &mut Self) {}
+
+    pub fn move_pos(&mut self) {
+        self.pos += self.vel;
+    }
+
+    pub fn border_check(&mut self) {}
+
+    pub fn draw(&self) {
+        draw_circle(self.pos.x, self.pos.y, RADIUS, BLUE);
+    }
 }
