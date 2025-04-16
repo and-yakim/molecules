4b0baf63eb46a82e8cf04292967f63744012f62f
Andrii Yakymenko <59168235+and-yakim@users.noreply.github.com>
2025-04-16 22:31:54 +0300

    created default gas struct

--- src/gas.rs
+++ src/gas.rs
@@ -1,3 +1,78 @@
-use super::binned_arr::*;
-use super::molecule::*;
+pub use super::binned_arr::*;
+pub use super::molecule::*;
 use super::*;
+
+type Particle = Atom<20>;
+const SIZE: f32 = 800.0;
+const CELL: f32 = Particle::RC;
+const FAR_BORDER: f32 = CELL + SIZE;
+
+pub struct Gas {
+    pub value: Vec<Particle>,
+    pub system: BinnedArr<usize>,
+}
+
+impl Gas {
+    pub fn new(sparcity: f32) -> Gas {
+        let value = Particle::generate(SIZE, Vec2::splat(CELL), sparcity);
+        let system = BinnedArr::<usize>::new(SIZE, CELL, value.len());
+        Gas { value, system }
+    }
+
+    pub fn refresh_sys(&mut self) {
+        self.system.clear();
+        for (i, mol) in self.value.iter().enumerate() {
+            self.system.add(mol.pos, i);
+        }
+    }
+
+    pub fn force_gas(&mut self) {
+        for i in 0..self.system.side {
+            for j in 0..self.system.side {
+                self.system.update_by_fn([i, j], |x, y| {
+                    if let Some(force) = self.value[*x].get_force(&self.value[*y]) {
+                        self.value[*x].vel += force;
+                        self.value[*y].vel -= force;
+                    }
+                });
+            }
+        }
+    }
+
+    pub fn move_gas(&mut self) {
+        for mol in self.value.iter_mut() {
+            mol.move_pos();
+        }
+
+        for n in 0..self.system.side {
+            self.system.arr[[n, 0]].iter().for_each(|i| {
+                if self.value[*i].pos.x < self.system.cell {
+                    self.value[*i].pos.x += SIZE;
+                }
+            });
+            self.system.arr[[n, self.system.side - 1]]
+                .iter()
+                .for_each(|i| {
+                    if self.value[*i].pos.x > FAR_BORDER {
+                        self.value[*i].pos.x -= SIZE;
+                    }
+                });
+            self.system.arr[[0, n]].iter().for_each(|i| {
+                if self.value[*i].pos.y < self.system.cell {
+                    self.value[*i].pos.y += SIZE;
+                }
+            });
+            self.system.arr[[self.system.side - 1, n]]
+                .iter()
+                .for_each(|i| {
+                    if self.value[*i].pos.y > FAR_BORDER {
+                        self.value[*i].pos.y -= SIZE;
+                    }
+                });
+        }
+    }
+
+    pub fn draw(&self) {
+        self.value.iter().for_each(Molecule::draw);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,24 @@
 use init::*;
-use molecules::binned_arr::*;
-use molecules::molecule::*;
+use molecules::gas::*;
 use molecules::*;
-
-const SIZE: f32 = 800.0;
-type Particle = Atom<20>;
 
 #[macroquad::main("Molecules")]
 async fn main() {
     init();
-    let cell = Particle::RC;
-    let far_border = cell + SIZE;
-
-    let mut gas = Particle::generate(SIZE, Vec2::splat(cell), 10.0);
-    let mut binarr = BinnedArr::<usize>::new(SIZE, cell, gas.len());
-    println!("N: {}", gas.len());
-    let camera = binarr.get_camera();
+    let mut gas = Gas::new(10.0);
+    println!("N: {}", gas.value.len());
+    let camera = gas.system.get_camera();
 
     loop {
         clear_background(DARKGRAY);
         set_camera(&camera);
 
-        binarr.clear();
-        for (i, mol) in gas.iter().enumerate() {
-            binarr.add(mol.pos, i);
-        }
+        gas.refresh_sys();
 
-        (0..binarr.side).for_each(|i| {
-            (0..binarr.side).for_each(|j| {
-                binarr.update_by_fn([i, j], |x, y| {
-                    if let Some(force) = gas[*x].get_force(&gas[*y]) {
-                        gas[*x].vel += force;
-                        gas[*y].vel -= force;
-                    }
-                });
-            })
-        });
+        gas.force_gas();
+        gas.move_gas();
 
-        for mol in gas.iter_mut() {
-            mol.move_pos();
-        }
-
-        for n in 0..binarr.side {
-            binarr.arr[[n, 0]].iter().for_each(|i| {
-                if gas[*i].pos.x < binarr.cell {
-                    gas[*i].pos.x += SIZE;
-                }
-            });
-            binarr.arr[[n, binarr.side - 1]].iter().for_each(|i| {
-                if gas[*i].pos.x > far_border {
-                    gas[*i].pos.x -= SIZE;
-                }
-            });
-            binarr.arr[[0, n]].iter().for_each(|i| {
-                if gas[*i].pos.y < binarr.cell {
-                    gas[*i].pos.y += SIZE;
-                }
-            });
-            binarr.arr[[binarr.side - 1, n]].iter().for_each(|i| {
-                if gas[*i].pos.y > far_border {
-                    gas[*i].pos.y = SIZE;
-                }
-            });
-        }
-
-        for mol in gas.iter_mut() {
-            mol.draw();
-        }
+        gas.draw();
 
         if is_key_pressed(KeyCode::Escape) {
             break;
